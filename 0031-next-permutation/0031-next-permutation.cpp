9f69eb6879b7c5c84b732c0c569517e1d79204f7
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2025-01-09 17:34:15 +0530

    Time: 0 ms (100%), Space: 15.8 MB (23.41%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-
-int next_greater(vector<int>nums,int i){
-     for(int j=nums.size()-1;j>i-1;j--){
-              if(nums[j]>nums[i-1])  return j;
-            }
-            return i;
-      }
-
     void nextPermutation(vector<int>& nums) {
-        int k;
-        for(int i=nums.size()-1;i>0;i--)
-         {
-            if(nums[i-1]<nums[i]){
-                k=next_greater(nums,i);
-                swap(nums[i-1],nums[k]);
-                sort(nums.begin()+i,nums.end());
-                 return ;
-               }
-        
-          }
-        sort(nums.begin(),nums.end());
-    }
+        int ind = -1;
+        int n = nums.size();
 
+        // Step 1: Find the "dip"
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+
+        // Step 2: Handle edge case where the entire sequence is in descending order
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return; // Early exit since the sequence has been reversed
+        }
+
+        // Step 3: Find the element to swap with nums[ind]
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the elements after the index `ind`
+        reverse(nums.begin() + ind + 1, nums.end());
+    }
 };
