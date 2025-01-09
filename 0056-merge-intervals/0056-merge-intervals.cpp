8c754d64419e8f65f4589d5652e38b4b8e85337f
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2025-01-09 21:54:43 +0530

    Time: 8 ms (28.72%), Space: 23.6 MB (74.07%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //TC -> O(NLOGN) + O(2N)
         int n=intervals.size();
         vector<vector<int>>ans;
         sort(intervals.begin(),intervals.end());
         for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            if(!ans.empty() && start<=ans.back()[1]){
-                continue;
+
+            if(ans.empty()||ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
             }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end){
-                    end=max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
-            ans.push_back({start,end});
+            // int start=intervals[i][0];
+            // int end=intervals[i][1];
+            // if(!ans.empty() && start<=ans.back()[1]){
+            //     continue;
+            // }
+            // for(int j=i+1;j<n;j++){
+            //     if(intervals[j][0]<=end){
+            //         end=max(end,intervals[j][1]);
+            //     }
+            //     else{
+            //         break;
+            //     }
+            // }
+            // ans.push_back({start,end});
         }
         return ans;
     }
