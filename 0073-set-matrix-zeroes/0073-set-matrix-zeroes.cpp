bd7cba0f16074b3e9c5f0a02463c3bd3d2874e1e
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2025-06-17 13:43:56 +0530

    Time: 0 ms (100%), Space: 18.4 MB (95.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -24,30 +24,37 @@ public:
         //         }
         //         if(colO) matrix [i][0]=0;
         //     }
+        int m = matrix.size();
+        int n = matrix[0].size();
         int col0=1;
-        int m= matrix.size(), n= matrix[0].size();
         for(int i=0;i<m;i++){
             if(matrix[i][0]==0){
                 col0=0;
             }
             for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0; //mark the first column
-                    matrix[0][j]=0; //mark the first row
+                    matrix[0][j]=0; //for marking column -> mark the first row
+                    matrix[i][0]=0; //for marking row -> mark the first column
                 }
             }
         }
-        for( int i =m-1;i>=0;i--){
-            for(int j=n-1;j>=1;j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
                     matrix[i][j]=0;
                 }
             }
-            if (col0==0){
+        }
+        if(matrix[0][0]==0){
+            for(int j=1;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<m;i++){
                 matrix[i][0]=0;
             }
         }
-
-        }
+    }
     
 };
