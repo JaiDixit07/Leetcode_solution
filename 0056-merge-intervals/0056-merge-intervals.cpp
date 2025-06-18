e107a4e05d3fa1f35573ddc9431d44c498a74dc1
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2025-06-18 13:40:32 +0530

    Time: 5 ms (55.97%), Space: 23.6 MB (95.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -9,13 +9,13 @@ public:
 
             if(ans.empty()||ans.back()[1]<intervals[i][0]){
                 ans.push_back(intervals[i]);
-            }
+            }  // TC -> 0(N) + O(NlogN)  -> single pass
             else{
                 ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
             // int start=intervals[i][0];
             // int end=intervals[i][1];
-            // if(!ans.empty() && start<=ans.back()[1]){
+            // if(!ans.empty() && end<=ans.back()[1]){
             //     continue;
             // }
             // for(int j=i+1;j<n;j++){
