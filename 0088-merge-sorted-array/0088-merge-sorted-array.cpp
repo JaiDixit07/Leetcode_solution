6b53bc1d7654fb858bdd23f15319045c738a0023
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2025-06-18 14:24:15 +0530

    Time: 0 ms (100%), Space: 12.1 MB (95.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -18,14 +18,24 @@ public:
         // }
         // sort(nums1.begin(),nums1.end());
         // sort(nums2.begin(),nums2.end());
-        // for(int i=0;i<n;i++){
-        //     nums1.insert
-        // }
-        for (int i = 0; i < n; ++i) {
-            nums1[m + i] = nums2[i];
-        }
+//------------------------------------------------------------
+    //     for (int i = 0; i < n; i++) {
+    //         nums1[m + i] = nums2[i];
+    //     }
 
-    // Sort nums1 to merge arrays
-    sort(nums1.begin(), nums1.end());
+    // // Sort nums1 to merge arrays
+    // sort(nums1.begin(), nums1.end());
+//_____________________________________________________________
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
     }
 };
