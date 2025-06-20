88add75be684486253ef6483d91184de85802c9f
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2025-06-20 20:27:20 +0530

    Time: 48 ms (63.05%), Space: 28.8 MB (99.93%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,22 +2,34 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         // Better Approach
+        // 0(N^2 + logM)
+        // Is giving tle
+        vector<vector<int>> ans;
         int n=nums.size();
-        set<vector<int>> st;
-        for(int i=0;i<n;i++){
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
 
-            set<int> hashset;
-            for(int j=i+1;j<n;j++){
-                int k= -(nums[i]+nums[j]);
-                if(hashset.find(k)!=hashset.end()){
-                    vector<int>temp={nums[i],nums[j],k};
-                    sort(temp.begin(),temp.end());
-                    st.insert(temp);
+            while(j<k){
+                int target=nums[i]+nums[j]+nums[k];
+                if(target>0){
+                    k--;
                 }
-                hashset.insert(nums[j]);
+                else if(target<0){
+                    j++;
+                }
+                else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
             }
         }
-        vector<vector<int>> ans(st.begin(),st.end());
-        return ans;        
+        return ans;
+
     }
 };
