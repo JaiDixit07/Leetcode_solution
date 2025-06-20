7052763a214fc1d4314bef0ad01dcfe58780957c
Jai Dixit <157274687+JaiDixit07@users.noreply.github.com>
2025-06-20 20:27:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
+        // Better Approach
+        int n=nums.size();
+        set<vector<int>> st;
+        for(int i=0;i<n;i++){
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+            set<int> hashset;
+            for(int j=i+1;j<n;j++){
+                int k= -(nums[i]+nums[j]);
+                if(hashset.find(k)!=hashset.end()){
+                    vector<int>temp={nums[i],nums[j],k};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
                 }
+                hashset.insert(nums[j]);
             }
         }
-        return res;        
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;        
     }
 };
